60d7db38e92e4123c7d7189c7c3f18da0547862b
shubhampatil2308 <shubhampatil68824@gmail.com>
2026-01-01 17:24:35 +0530

    Initial commit - Smart Hostel project

---
+++ src/test/java/com/SmartHostel/SmartHostelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartHostel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHostelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
